c7602a33576ac7797805bca0de05a513b863f01b
sadneo <haywardeel@gmail.com>
2024-03-15 06:40:45 +0000

    Client updates server when it reads from stdin

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,6 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::task;
 
 pub async fn serve(sock_addr: (&str, u16)) -> std::io::Result<()> {
@@ -25,18 +26,20 @@ async fn serve_stream(mut stream: TcpStream) -> std::io::Result<()> {
 }
 
 pub async fn client(sock_addr: (&str, u16)) -> std::io::Result<()> {
-    use std::io::{self, BufRead};
+    let (rstream, wstream) = TcpStream::connect(sock_addr).await?.into_split();
 
-    let mut stream = TcpStream::connect(sock_addr).await?;
-    let mut stdin = io::stdin().lock();
+    task::spawn(client_send(wstream));
+    Ok(())
+}
 
+async fn client_send(mut wstream: OwnedWriteHalf) -> std::io::Result<()> {
+    let mut stdin = io::stdin();
     loop {
-        let mut buffer = String::new();
-        stdin.read_line(&mut buffer)?;
-        let bytes = buffer.as_bytes();
-        let length = bytes.len().to_be_bytes();
+        let size = stdin.read_u64().await?;
+        let mut buffer = vec![0; size as usize];
+        stdin.read_exact(&mut buffer).await?;
 
-        stream.write_all(&length).await?;
-        stream.write_all(bytes).await?;
+        wstream.write_all(&size.to_be_bytes()).await?;
+        wstream.write_all(&buffer).await?;
     }
 }
