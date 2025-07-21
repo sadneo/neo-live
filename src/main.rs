89d657fdd3b19302fba71a62a46b90852d511099
sadneo <haywardeel@gmail.com>
2025-07-21 09:55:52 -0400

    feat: one-way file sharing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use clap::{Parser, Subcommand};
 use std::fs;
 use std::io::{Read, Write};
-use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
 use std::path::PathBuf;
 use std::str::FromStr;
-use std::sync::mpsc::{self, Receiver};
+use std::sync::{Arc, RwLock};
 use std::thread;
 
-mod watchers;
+use notify::{Config, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 
 #[derive(Parser, Debug)]
 #[command(version, author, about)]
@@ -46,35 +46,70 @@ enum Command {
 async fn main() {
     let cli = Cli::parse();
 
-    let path = std::path::PathBuf::from("test.txt").canonicalize().unwrap();
-    let (file_update_sender, file_update_receiver) = mpsc::channel::<String>();
-    thread::spawn(move || {
-        watchers::watch(&path, file_update_sender);
-    });
-
     match cli.command {
-        Command::Serve { path } => todo!("{:?}", path),
-        Command::Connect { path, address } => connect(path, file_update_receiver, Ipv4Addr::from_str(&address).unwrap(), cli.server_port, cli.client_port),
+        Command::Serve { path } => serve(path, cli.server_port),
+        Command::Connect { path, address } => connect(
+            path,
+            Ipv4Addr::from_str(&address).expect("Expected address"),
+            cli.server_port,
+        ),
     }
 }
 
-fn connect(output: PathBuf, file_updates: Receiver<String>, remote: Ipv4Addr, server_port: u16, client_port: u16) {
-    let read_socket = SocketAddrV4::new(remote, client_port);
-    let write_socket = SocketAddrV4::new(remote, server_port);
+fn serve(file_path: PathBuf, server_port: u16) {
+    let address = "127.0.0.1".parse().unwrap();
+    let write_socket = SocketAddrV4::new(address, server_port);
+    let listener = TcpListener::bind(write_socket).expect("error when creating listener");
+
+    let clients: Arc<RwLock<Vec<TcpStream>>> = Arc::new(RwLock::new(Vec::new()));
+    let clients_ref = clients.clone();
+    let file_path_clone = file_path.clone();
 
     thread::spawn(move || {
-        let mut stream = TcpStream::connect(read_socket).unwrap();
-        let mut buffer = String::new();
-        loop {
-            stream.read_to_string(&mut buffer).unwrap();
-            fs::write(&output, &buffer).unwrap();
-            buffer.clear();
+        for stream in listener.incoming().flatten() {
+            let mut clients_guard = clients.write().unwrap();
+            println!("{:?}", stream);
+            clients_guard.push(stream);
         }
     });
-    thread::spawn(move || {
-        let mut stream = TcpStream::connect(write_socket).unwrap();
-        for update in file_updates {
-            stream.write_all(update.as_bytes()).unwrap();
+
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher: RecommendedWatcher =
+    Watcher::new(tx, Config::default()).expect("Failed to create watcher");
+
+    watcher
+        .watch(&file_path_clone, RecursiveMode::NonRecursive)
+        .expect("Failed to watch file");
+
+    loop {
+        if let Ok(event) = rx.recv() {
+            if let EventKind::Modify(_) = event.unwrap().kind {
+                println!("modify");
+                let Ok(contents) = fs::read(&file_path) else {
+                    continue;
+                };
+
+                let mut guard = clients_ref.write().unwrap();
+                guard.retain_mut(|stream| {
+                    stream.write_all(&contents).is_ok()
+                });
+            }
         }
-    });
+    }
+}
+
+fn connect(output: PathBuf, remote: Ipv4Addr, server_port: u16) {
+    let read_socket = SocketAddrV4::new(remote, server_port);
+
+    let mut stream = TcpStream::connect(read_socket).unwrap();
+    let mut buffer = [0; 500];
+    loop {
+        let n = stream.read(&mut buffer).unwrap();
+        if n == 0 {
+            break;
+        }
+        let contents = String::from_utf8_lossy(&buffer[..n]);
+        println!("recv: {:?}", contents);
+        fs::write(&output, &buffer).unwrap();
+    }
 }
