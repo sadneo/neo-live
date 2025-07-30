95d7d89c7d76d2ce8290dfc4e05dff7bd6d2fc0f
sadneo <haywardeel@gmail.com>
2025-07-30 00:07:06 -0400

    feat: serve use sync, some abstraction added utilities for framing easier, might think out better solution later

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
-use std::fs;
-use std::path::PathBuf;
+use std::net::{Ipv4Addr, SocketAddrV4};
 use std::str::FromStr;
-
-use std::thread;
-use std::sync::{Arc, RwLock};
-use std::io::{Write};
-use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
-
-use tokio::net::TcpStream as TokioTcpStream;
-use tokio::io::BufReader as TokioBufReader;
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use std::sync::Arc;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc::{self, Sender};
+use tokio::sync::RwLock;
+use tokio::task;
 
 use clap::{Parser, Subcommand, ValueEnum};
-use notify::{Config, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
-use rmp_serde::{encode};
 use serde::{Deserialize, Serialize};
+
+const CHANNEL_SIZE: usize = 5;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct TextUpdate {
     text: String,
+}
+
+impl TextUpdate {
+    fn encode(self) -> Option<Vec<u8>> {
+        let mut buf = Vec::new();
+        let payload = match rmp_serde::to_vec(&self) {
+            Ok(p) => p,
+            Err(e) => {
+                eprintln!("msgpack encode error: {e}");
+                return None;
+            }
+        };
+
+        buf.clear();
+        let len = (payload.len() as u32).to_be_bytes();
+        buf.extend_from_slice(&len);
+        buf.extend_from_slice(&payload);
+
+        Some(buf)
+    }
 }
 
 #[derive(ValueEnum, Clone, Debug)]
@@ -43,9 +59,6 @@ struct Cli {
 enum Command {
     /// Serve file to a socket
     Serve {
-        /// File to serve
-        path: PathBuf,
-
         // currently defaults to HostMode::Local for debugging
         /// Interface to bind to
         #[arg(long, value_enum, default_value_t = HostMode::Local)]
@@ -59,12 +72,44 @@ enum Command {
     },
 }
 
+struct FrameReader<T> {
+    reader: BufReader<T>,
+}
+
+impl<R: tokio::io::AsyncRead + Unpin> FrameReader<R> {
+    fn new(reader: R) -> FrameReader<R> {
+        let reader = BufReader::new(reader);
+        Self { reader }
+    }
+    async fn read_loop(mut self, sender: Sender<Vec<u8>>) {
+        let mut len_buf = [0u8; 4];
+        loop {
+            if self.reader.read_exact(&mut len_buf).await.is_err() {
+                eprintln!("Connection closed");
+                break;
+            }
+            let len = u32::from_be_bytes(len_buf) as usize;
+            eprintln!("{}", len);
+
+            let mut buf = vec![0u8; len];
+            if self.reader.read_exact(&mut buf).await.is_err() {
+                eprintln!("Connection closed");
+                break;
+            }
+            if sender.send(buf).await.is_err() {
+                eprintln!("Channel closed");
+                break;
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::Serve { path, host_mode } => serve(path, host_mode, cli.port).await,
+        Command::Serve { host_mode } => serve(host_mode, cli.port).await,
         Command::Connect { address } => {
             connect(
                 Ipv4Addr::from_str(&address).expect("Expected address"),
@@ -75,7 +120,7 @@ async fn main() {
     }
 }
 
-async fn serve(file_path: PathBuf, host_mode: HostMode, port: u16) {
+async fn serve(host_mode: HostMode, port: u16) {
     let address = match host_mode {
         HostMode::Local => Ipv4Addr::new(127, 0, 0, 1),
         HostMode::All => Ipv4Addr::new(0, 0, 0, 0),
@@ -89,52 +134,50 @@ async fn serve(file_path: PathBuf, host_mode: HostMode, port: u16) {
 
     let clients: Arc<RwLock<Vec<TcpStream>>> = Arc::new(RwLock::new(Vec::new()));
     let clients_ref = clients.clone();
-    let file_path_clone = file_path.clone();
 
-    thread::spawn(move || {
-        let listener = TcpListener::bind(write_socket).expect("Failed to create listener");
-        for stream in listener.incoming().flatten() {
-            let mut clients_guard = clients.write().unwrap();
-            clients_guard.push(stream);
-            eprintln!("Client added");
+    tokio::task::spawn(async move {
+        let listener = TcpListener::bind(write_socket)
+            .await
+            .expect("Failed to create Listener");
+        loop {
+            match listener.accept().await {
+                Ok((stream, address)) => {
+                    let mut clients_guard = clients_ref.write().await;
+                    clients_guard.push(stream);
+                    eprintln!("Client added at {}", address);
+                }
+                Err(e) => {
+                    eprintln!("listener: {:?}", e);
+                    break;
+                }
+            }
         }
     });
 
-    // TODO: instead of watcher, listen to stdin instead and send the the entire buffer through msgpack
-    // instead of a notify Event
-    let (tx, rx) = std::sync::mpsc::channel();
-    let mut watcher: RecommendedWatcher =
-        Watcher::new(tx, Config::default()).expect("Failed to create watcher");
+    let stdin = io::stdin();
+    let reader = FrameReader::new(stdin);
+    let (tx, mut rx) = mpsc::channel(CHANNEL_SIZE);
 
-    watcher
-        .watch(&file_path_clone, RecursiveMode::NonRecursive)
-        .expect("Failed to watch file");
+    tokio::task::spawn(async move { reader.read_loop(tx) });
 
-    let mut buf = Vec::new();
     loop {
-        if let Ok(event) = rx.recv() {
-            if let EventKind::Modify(_) = event.unwrap().kind {
-                eprintln!("modify");
-                let Ok(contents) = fs::read_to_string(&file_path) else {
-                    continue;
-                };
-                let message = TextUpdate { text: contents };
+        let Some(msg_bytes) = rx.recv().await else {
+            eprintln!("Something went wrong");
+            break;
+        };
+        let message: TextUpdate = rmp_serde::from_slice(&msg_bytes).unwrap();
+        let framed_msg = message.encode().unwrap();
 
-                let payload = match encode::to_vec(&message) {
-                    Ok(p) => p,
-                    Err(e) => {
-                        eprintln!("msgpack encode error: {e}");
-                        continue;
-                    }
-                };
-
-                buf.clear();
-                let len = (payload.len() as u32).to_be_bytes();
-                buf.extend_from_slice(&len);
-                buf.extend_from_slice(&payload);
-
-                let mut guard = clients_ref.write().unwrap();
-                guard.retain_mut(|stream| stream.write_all(&buf).is_ok());
+        // write to all streams, retain ones that don't error
+        let mut clients_guard = clients.write().await;
+        let mut i = 0;
+        while i < clients_guard.len() {
+            let stream = &mut clients_guard[i];
+            match stream.write_all(&framed_msg).await {
+                Ok(_) => i += 1,
+                Err(_) => {
+                    clients_guard.remove(i);
+                }
             }
         }
     }
@@ -142,24 +185,22 @@ async fn serve(file_path: PathBuf, host_mode: HostMode, port: u16) {
 
 async fn connect(remote: Ipv4Addr, port: u16) {
     let read_socket = SocketAddrV4::new(remote, port);
-    let stream = TokioTcpStream::connect(read_socket).await.unwrap();
-    let mut reader = TokioBufReader::new(stream);
+    let stream = TcpStream::connect(read_socket).await.unwrap();
+    let reader = FrameReader::new(stream);
     let mut stdout = io::stdout();
+    let (tx, mut rx) = mpsc::channel(CHANNEL_SIZE);
 
-    let mut len_buf = [0u8; 4];
+    task::spawn(async move { reader.read_loop(tx) });
+
     loop {
-        if reader.read_exact(&mut len_buf).await.is_err() {
-            eprintln!("Connection closed");
+        let Some(msg_bytes) = rx.recv().await else {
+            eprintln!("Something went wrong");
             break;
-        }
-        let len = u32::from_be_bytes(len_buf) as usize;
-        eprintln!("{}", len);
-
-        let mut buf = vec![0u8; len];
-        reader.read_exact(&mut buf).await.unwrap();
-        let message = rmp_serde::from_slice::<TextUpdate>(&buf).unwrap();
+        };
+        let message: TextUpdate = rmp_serde::from_slice(&msg_bytes).unwrap();
         eprintln!("message: {:?}", message);
-        
-        stdout.write_all(&buf).await.expect("IO error");
+
+        let framed_msg = message.encode().unwrap();
+        stdout.write_all(&framed_msg).await.expect("IO error");
     }
 }
