083bed3874fb09bf7e963e0e4cbd0fa30f3a6876
sadneo <haywardeel@gmail.com>
2026-01-02 19:50:24 -0500

    chore: nix flake added

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Rust development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-analyzer
+            cargo
+            rustc
+            rustfmt
+          ];
+        };
+    };
+}
